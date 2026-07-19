711a293133486c4787a8ba07cc25eb82bd4ae1f0
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-07-19 04:51:21 +0430

    Stage 4: full CRUD

--- BE-01/index.js
+++ BE-01/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
@@ -7,53 +7,91 @@ app.use(express.json());
 const port = 3000;
 
 var tasks = [
-    {"id": 1, "title": "Journal Writing", "done": "false"},
-    {"id": 2, "title": "Exercise", "done": "true"},
-    {"id": 3, "title": "Read a book", "done": "false"},
-]
+  { id: 1, title: "Journal Writing", done: "false" },
+  { id: 2, title: "Exercise", done: "true" },
+  { id: 3, title: "Read a book", done: "false" },
+];
 
 app.get("/", (req, res) => {
-    res.json({"name": "Task API","version": "1.0", "endpoints": ["/tasks"] });
+  res.json({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
 });
 
 app.get("/health", (req, res) => {
-    res.json({"done": "ok"});
+  res.json({ done: "ok" });
 });
 
 app.get("/tasks", (req, res) => {
-    res.json(tasks);
+  res.json(tasks);
 });
 
 app.get("/tasks/:id", (req, res) => {
-    const taskId = parseInt(req.params.id);
-    
-    const task = tasks.find(task => task.id === taskId);
-    if(task)
-    {
-        res.json(task);
-    }
-    res.status(404).json({"error": `Task ${taskId} not found`});    
-    
+  const taskId = parseInt(req.params.id);
+
+  const task = tasks.find((task) => task.id === taskId);
+  if (task) {
+    res.json(task);
+  }
+  res.status(404).json({ error: `Task ${taskId} not found` });
 });
 
-app.post("/tasks", (req, res) =>{
-    const {title} = req.body;
-    if(!title || title.trim() === "")
-    {
-        res.status(400).json({"error": "Title is required"});
-    }
+app.post("/tasks", (req, res) => {
+  const { title } = req.body;
+  if (!title || title.trim() === "") {
+    res.status(400).json({ error: "Title is required" });
+  }
 
-    const newTask = {
-        id: tasks.length + 1,
-        title: title,
-        done: "false"
-    };
+  const newTask = {
+    id: tasks.length + 1,
+    title: title,
+    done: "false",
+  };
 
-    tasks.push(newTask);
+  tasks.push(newTask);
 
-    res.json(newTask);
-})
+  res.json(newTask);
+});
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-})
+app.put("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const { title, done } = req.body;
+
+  if (
+    title === undefined ||
+    done === undefined ||
+    (title.trim() === "" && done.trim() === "")
+  ) {
+    res.status(400).json({ error: "Title and done are required" });
+  }
+
+  const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+  if (taskIndex === -1) {
+    res.status(404).json({ error: `Task ${taskId} not found` });
+  }
+  if (title.trim() !== "") {
+    tasks[taskIndex].title = title;
+  }
+  if (done.trim() !== "") {
+    tasks[taskIndex].done = done;
+  }
+
+  res.json(tasks[taskIndex]);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+  if (taskIndex === -1) {
+    res.status(404).json({ error: `Task ${taskId} not found` });
+  }
+
+  tasks.splice(taskIndex, 1);
+
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
