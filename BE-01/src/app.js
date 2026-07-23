e424045b462431fe03f7dc52818e59e5daec2d0e
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-07-23 19:09:03 +0430

    Layered Architecture: add app.js and error.js

--- BE-01/src/app.js
+++ BE-01/src/app.js
@@ -1,4 +1,27 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import specs from "./openapi.json" with { type: "json" };
+import specs from "../openapi.json" with { type: "json" };
+import tasksRouter from "./routes/tasks.router.js";
+import errorHandler from "./middleware/errorHandler.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+
+app.get("/", (req, res) => {
+	res.json({ name: "Task API", version: "1.0", endpoints: ["/tasks"] });
+});
+
+app.get("/health", (req, res) => {
+	res.json({ done: "ok" });
+});
+
+app.use("/tasks", tasksRouter);
+
+// error handler should be last
+app.use(errorHandler);
+
+export default app;
 
