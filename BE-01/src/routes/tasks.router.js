e6b94fde1c70aa973f67e886b479d6febd45a124
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-07-23 19:07:55 +0430

    Layered Architecture: Add routes

---
+++ BE-01/src/routes/tasks.router.js
@@ -0,0 +1,58 @@
+import express from "express";
+import {
+  listTasks,
+  getTask,
+  createNewTask,
+  updateExistingTask,
+  deleteExistingTask,
+} from "../services/tasks.services";
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  try {
+    const data = listTasks();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", (req, res, next) => {
+  try {
+    const { title } = req.body;
+    const created = createNewTask(title);
+    res.status(201).json(created);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", (req, res, next) => {
+  try {
+    const task = getTask(req.params.id);
+    res.json(task);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", (req, res, next) => {
+  try {
+    const updated = updateExistingTask(req.params.id, req.body);
+    res.json(updated);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", (req, res, next) => {
+  try {
+    deleteExistingTask(req.params.id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
