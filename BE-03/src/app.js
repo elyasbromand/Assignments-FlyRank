eaf6727eb71b3e7a4c60a7f2b4b661845783f827
Elyas Bromand <elyasbromand@users.noreply.github.com>
2026-07-30 17:42:39 +0430

    Stage 5: Swagger UI documentation with bearer auth

--- BE-03/src/app.js
+++ BE-03/src/app.js
@@ -4,12 +4,17 @@ import errorHandler from "./middleware/errors.middleware.js";
 import authRouter from "./routes/auth.router.js";
 import publicRouter from "./routes/public.router.js";
 import protectedRouter from "./routes/protected.router.js";
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
 
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+const openapiSpec = JSON.parse(fs.readFileSync(new URL('../openapi.json', import.meta.url)));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 
 app.use("/auth", authRouter);
